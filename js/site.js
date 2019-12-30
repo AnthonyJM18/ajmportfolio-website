d75b4b6a327426d4f7d1ece9188419650cdd796c
Anthony Merante <44046970+AnthonyJM18@users.noreply.github.com>
2019-12-30 17:05:26 -0500

    1.3
    Added Contact Modal,
    Changed favicon.ico
    Updated TODO

--- js/site.js
+++ js/site.js
@@ -1,4 +1,44 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(function () {
 
-// Write your Javascript code.
+    const buildModal = () => {
+        div = $(`
+               <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header d-block">
+                    <button type="button" class="close float-right" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                    <h1 class="modal-title text-center" id="modaltitle">Contact Information</h1>
+                </div>
+                <div class="modal-body">
+                    <div class="card-body">
+                        <div class="row mb-2">
+                            <div class="col-2">Email: </div>
+                            <div class="col-4">
+                                <a href="mailto:anthonymerante@live.ca">anthonymerante@live.ca</a>
+                            </div>
+                        </div>
+                        <div class="row mb-2">
+                            <div class="col-2">Phone: </div>
+                            <div class="col-3">
+                                <a href="tel:9053416978">(905) 341-6978</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer text-center">
+                    <input type="button" value="Close" data-dismiss="modal" class="btn-secondary btn-lg float-left" />
+                </div>
+            </div>
+        </div>  
+        `);
+        $("#contactModal").append(div);
+    };
+
+    $("#contact").click(() => {
+        $("#contactModal").modal("toggle");
+    });
+
+    buildModal();
+
+});
